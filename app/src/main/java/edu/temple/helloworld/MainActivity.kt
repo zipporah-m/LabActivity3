99620079e8fc751b5dda13c2c5f76869342ac0a3
zipporah-m <tuu10382@temple.edu>
2026-09-23 16:20:54 -0400

    app is working properly now

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -20,7 +20,11 @@ class MainActivity : AppCompatActivity() {
 
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            if((findViewById<EditText>(R.id.nameEditText).text).isNotEmpty()){
+                    displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+                } else {
+                    displayTextView.text = "Please enter your name"
+            }
         }
 
 
